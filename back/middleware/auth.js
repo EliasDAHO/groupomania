2f787c3c0c5258030ac814db883f750942d42721
EliasDAHO <87639332+EliasDAHO@users.noreply.github.com>
2022-02-24 17:04:06 +0100

    modif

--- back/middleware/auth.js
+++ back/middleware/auth.js
@@ -6,7 +6,7 @@ module.exports = (req, res, next) => {
     const token = req.headers.authorization.split(' ')[1];
     const decodedToken = jwt.verify(token,process.env.TOKEN_SECRET);
     const userId = decodedToken.userId;
-    const role = decodedToken.role;
+    
     if (req.body.userId && req.body.userId !== userId) {
       throw 'Invalid user ID';
     } else {
