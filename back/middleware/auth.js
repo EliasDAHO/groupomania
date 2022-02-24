00e93d427204ccbac55af74c4f99af73e1e2dc01
EliasDAHO <87639332+EliasDAHO@users.noreply.github.com>
2022-02-24 10:54:27 +0100

    modif

--- back/middleware/auth.js
+++ back/middleware/auth.js
@@ -6,14 +6,14 @@ module.exports = (req, res, next) => {
     const token = req.headers.authorization.split(' ')[1];
     const decodedToken = jwt.verify(token,process.env.TOKEN_SECRET);
     const userId = decodedToken.userId;
+    const role = decodedToken.role;
     if (req.body.userId && req.body.userId !== userId) {
       throw 'Invalid user ID';
     } else {
       next();
     }
-  } catch {
-    res.status(401).json({
-      error: new Error('Invalid request!')
-    });
+  } catch (error)  {
+    res.status(401).json({ error:error | 'Invalid request!'});
+      
   }
 };
