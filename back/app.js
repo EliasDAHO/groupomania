00222c3cdd55f9d9b21555fa073d746a29083018
EliasDAHO <87639332+EliasDAHO@users.noreply.github.com>
2022-02-24 17:06:25 +0100

    modif

--- back/app.js
+++ back/app.js
@@ -1,46 +1,40 @@
 const express = require('express');
-const helmet = require('helmet');
-const path = require('path');
 const bodyParser = require('body-parser');
-const { sequelize } = require('./models');
+const { sequelize }=  require('./models');
+const path = require("path")
 
 
 const userRoutes = require('./routes/user');
 const postRoutes = require('./routes/post');
-const commentRoutes = require('./routes/comment');
+const commentsRoutes = require('./routes/comment') 
+ 
+ const connect = async function() {
+    try{
+       await sequelize.authenticate();
+        console.log('Connection has been successfully established !'); 
+     }
+     catch (error) {
+        console.error('Unable to connect to the database !')
+     }
+ };
+ connect();
 
 
-const connect = async function () {
-    try {
-      await sequelize.authenticate();
-      console.log('La connexion a été établie avec succès à SQL.');
-    } catch (error) {
-      console.error('Impossible de se connecter à la base de données SQL');
-    }
-  };
-  connect();
-
-  const app = express(); 
+const app = express();
 
 app.use((req, res, next) => {
     res.setHeader('Access-Control-Allow-Origin', '*');
     res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content, Accept, Content-Type, Authorization');
     res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, PATCH, OPTIONS');
     next();
-});
+  });
 
-
-app.use(helmet());
 app.use(express.json());
 app.use(bodyParser.json());
-app.use('/images', express.static(path.join(__dirname, 'images')));
+app.use('/images',express.static(path.join(__dirname,'images')));
 
-app.use('/api/comments', commentRoutes);
-app.use('/api/posts', postRoutes);
-app.use('/api/auth', userRoutes);
-
-
-
-
+app.use('/api/auth',userRoutes);
+app.use('/api/posts',postRoutes);
+app.use('/api/comments',commentsRoutes);
 
 module.exports = app;
